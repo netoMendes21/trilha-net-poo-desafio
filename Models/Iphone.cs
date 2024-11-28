f78ff7f4de5bea0cd48856b11d27d6dcc619373e
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2024-11-28 16:17:01 -0300

    Desafio concluído

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        // TODO: Herdar da classe "Smartphone"
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            Console.WriteLine($"O iphone que tem o numero: {numero}, modelo: {modelo}, com o IMEI: {imei}, e armazenamento de: {memoria}Gb");
+
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o {nomeApp} no Iphone");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nesta Black Friday compre o Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia X900", imei: "111222333", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Nesta Black Friday compre o Iphone:");
+Smartphone iphone = new Iphone(numero: "654321", modelo: "Iphone 12 pro Max", imei: "333222111", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Itunes");
